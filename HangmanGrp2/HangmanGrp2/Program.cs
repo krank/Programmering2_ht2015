0f6dc22fed23b2c26b29f2d9a445e0e5d661dd7f
Mikael Bergström <krank23@gmail.com>
2015-09-09 10:38:25 +0200

    SMall update

---
+++ HangmanGrp2/HangmanGrp2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HangmanGrp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string correctWord = "krank";
+
+            char[] underscores = new char[correctWord.Length];
+
+            for (int i = 0; i < underscores.Length; i++)
+            {
+                underscores[i] = '_';
+            }
+
+            writeCharArray(underscores);
+
+            string guessTemp = Console.ReadLine();
+            char guess = guessTemp[0];
+
+            
+
+            Console.ReadLine();
+        }
+
+        static void writeCharArray(char[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
