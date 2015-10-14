db10fd01ef3945ed5ccf91731078258d8b01002b
Mikael Bergström <krank23@gmail.com>
2015-10-14 11:09:54 +0200

    Grp2 made asteroids

---
+++ TopdownShooter_grp2/Assets/AsteroidController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsteroidController : MonoBehaviour {
+
+    Vector3 toMove;
+
+	// Use this for initialization
+	void Start () {
+        float speed = Random.Range(0.01f, 0.05f);
+        toMove = new Vector3(0, -speed);
+
+        float randomX = Random.Range(-3f, 3f);
+        Vector3 newPos = new Vector3(randomX, 5);
+
+        this.transform.position = newPos;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Translate(toMove);
+
+        if (this.transform.position.y < -6)
+        {
+            Destroy(this.gameObject);
+            Debug.Log("NOOOOOOOOO");
+        }
+	}
+}
---
+++ TopdownShooter_grp2/Assets/AsteroidGenerator.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsteroidGenerator : MonoBehaviour {
+
+    public GameObject asteroid;
+
+    float timer = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+
+        if (timer > 2)
+        {
+            timer = 0;
+            Instantiate(asteroid);
+        }
+	}
+}
