84173c19a39cd0191c629d47d3fe9682e00a4f20
Mikael Bergström <krank23@gmail.com>
2015-11-09 08:03:21 +0100

    Implementation of level restart controller

---
+++ TopdownShooter_grp1/Assets/RestartController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RestartController : MonoBehaviour {
+
+    public void RestartScene()
+    {
+        Application.LoadLevel(Application.loadedLevelName);
+    }
+}
