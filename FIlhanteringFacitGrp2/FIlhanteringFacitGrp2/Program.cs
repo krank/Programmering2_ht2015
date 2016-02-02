d4d2552597456725ef7cb0d136b6a77bf413d509
Mikael Bergström <krank23@gmail.com>
2016-02-02 09:54:46 +0100

    Bomberman och filhantering
    Signed-off-by: Mikael Bergström <krank23@gmail.com>

---
+++ FIlhanteringFacitGrp2/FIlhanteringFacitGrp2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIlhanteringFacitGrp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListFromFile("test.txt");
+            Console.ReadLine();
+        }
+
+        public static List<string> ListFromFile(string fileName)
+        {
+            String content = System.IO.File.ReadAllText(@fileName);
+
+            string[] rows = content.Split('\n');
+
+            List<string> rowList = new List<string>();
+
+            for (var i=0; i<rows.Length; i++)
+            {
+
+                if (rows[i].Length > 1)
+                {
+                    rowList.Add(rows[i]);
+                }
+            }
+
+            Console.WriteLine(rowList.Count);
+
+            return rowList;
+        }
+    }
+}
---
+++ FilhanteringDemo/FilhanteringDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilhanteringDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = System.IO.File.ReadAllText(@"TextFile1.txt");
+
+            string[] rows = s.Split('\n');
+
+            List<string> wordsList = new List<string>(rows);
+
+
+            Console.WriteLine(wordsList[0]);
+
+            wordsList.Add("Micke");
+
+            string outLines = String.Join("\n", wordsList.ToArray());
+
+            Console.WriteLine(outLines);
+
+            System.IO.File.WriteAllText(@"TextFile1.txt", outLines);
+
+            Console.ReadLine();
+        }
+    }
+}
