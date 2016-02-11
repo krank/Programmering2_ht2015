71ebde972d002c3d795de45e5620f5628dde684e
Mikael Bergström <krank23@gmail.com>
2016-02-11 10:02:09 +0100

    Small update, fixed corruption
    Signed-off-by: Mikael Bergström <krank23@gmail.com>

---
+++ Bomberman/BombermanGrp2/Assets/Grid.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grid : MonoBehaviour {
+
+    int sizeX = 3;
+    int sizeY = 3;
+    float sizeOfTile = 0.5f;
+    GameObject[,] objects;
+
+	// Use this for initialization
+	void Start () {
+        objects = new GameObject[sizeX, sizeY];
+
+	}
+	
+}
