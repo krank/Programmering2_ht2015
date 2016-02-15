cb379138c68dfdd2e9f58fb8ec3a466b6f33802d
Mikael Bergström <krank23@gmail.com>
2016-02-15 09:45:59 +0100

    Gräslig uppdatering grp2
    Signed-off-by: Mikael Bergström <krank23@gmail.com>

--- Bomberman/BombermanGrp2/Assets/Grid.cs
+++ Bomberman/BombermanGrp2/Assets/Grid.cs
@@ -1,17 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Grid : MonoBehaviour {
+public class Grid : MonoBehaviour
+{
 
-    int sizeX = 3;
-    int sizeY = 3;
+    public int sizeX = 3;
+    public int sizeY = 3;
     float sizeOfTile = 0.5f;
     GameObject[,] objects;
 
-	// Use this for initialization
-	void Start () {
+    public GameObject prefab;
+
+    // Use this for initialization
+    void Start()
+    {
         objects = new GameObject[sizeX, sizeY];
 
-	}
-	
+
+        Generate();
+    }
+
+    public void Generate()
+    {
+
+    }
+
+    public void putObjectAt(int x, int y, GameObject obj)
+    {
+        Vector3 pos = new Vector3(x * sizeOfTile, y * sizeOfTile);
+
+        obj.transform.position = this.transform.position + pos;
+
+        objects[x, y] = obj;
+
+        obj.transform.SetParent(this.transform);
+    }
+
 }
