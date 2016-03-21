3dd2297911711af3a195ff84142f6ef4e8ddb418
Mikael Bergström <krank23@gmail.com>
2016-03-21 09:54:21 +0100

    Spacewar grp2 påbörjat.
    Signed-off-by: Mikael Bergström <krank23@gmail.com>

---
+++ SpaceWar_grp2/Assets/Grid.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Grid : MonoBehaviour
+{
+
+    public int sizeX = 3;
+    public int sizeY = 3;
+    public float sizeOfTile = 0.5f;
+    GameObject[,] objects;
+
+    public GameObject prefab;
+
+    // Use this for initialization
+    void Start()
+    {
+        objects = new GameObject[sizeX, sizeY];
+
+
+        Generate();
+    }
+
+    public virtual void Generate()
+    {
+
+    }
+
+    public void putObjectAt(int x, int y, GameObject obj)
+    {
+        Vector3 pos = new Vector3(x * sizeOfTile, 0, y * sizeOfTile);
+
+        obj.transform.position = this.transform.position + pos;
+
+        objects[x, y] = obj;
+
+        obj.transform.SetParent(this.transform);
+    }
+    
+    public void putObjectAt(Vector3 unityPos, GameObject obj)
+    {
+        Vector3 localPos = unityPos - this.transform.position;
+
+        int x = (int) Math.Round(localPos.x / sizeOfTile);
+        int y = (int)Math.Round(localPos.y / sizeOfTile);
+
+        putObjectAt(x, y, obj);
+        
+    }
+
+}
---
+++ SpaceWar_grp2/Assets/TileController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileController : MonoBehaviour {
+
+    Color ordinary = Color.gray;
+    Color mouseOver = Color.magenta;
+    Color canMove = Color.green;
+    Color cantMove = Color.red;
+
+    void Start()
+    {
+        Renderer r = this.GetComponent<Renderer>();
+
+        r.material.color = ordinary;
+    } 
+
+    void OnMouseEnter()
+    {
+        Renderer r = this.GetComponent<Renderer>();
+
+        r.material.color = mouseOver;
+    }
+
+    void OnMouseExit()
+    {
+        Renderer r = this.GetComponent<Renderer>();
+
+        r.material.color = ordinary;
+    }
+	
+}
